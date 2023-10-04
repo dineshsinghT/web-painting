29d12400a4a099ba20f565cd78cece2a3ad458f8
jatin jagwani <146410093+jatinjagwani@users.noreply.github.com>
2023-10-04 18:53:08 +0530

    Update main.js
    some major changes

--- main.js
+++ main.js
@@ -1,54 +1,38 @@
+// Initialize a cart object to store selected products
+const cart = {};
 
-    // Store tasks in an array
-    let tasks = [];
+// Function to add a product to the cart
+function addToCart(productId) {
+  if (cart[productId]) {
+    cart[productId]++;
+  } else {
+    cart[productId] = 1;
+  }
+}
 
-    // Function to add a new task
-    function addTask() {
-      const taskText = document.getElementById("new-task").value;
-      if (taskText.trim() !== "") {
-        tasks.push({ text: taskText, completed: false });
-        document.getElementById("new-task").value = "";
-        displayTasks();
-      }
-    }
+// Function to display the cart
+function showCart() {
+  const cartItems = document.getElementById('cart-items');
+  cartItems.innerHTML = '';
+  for (const productId in cart) {
+    const product = document.getElementById(product-${productId});
+    const productTitle = product.querySelector('.product-title').textContent;
+    const productPrice = product.querySelector('.product-price').textContent;
+    const cartItem = document.createElement('li');
+    cartItem.innerHTML = ${productTitle} - ${productPrice} x ${cart[productId]};
+    cartItems.appendChild(cartItem);
+  }
+  cartItems.style.display = 'block';
+}
 
-    // Function to display tasks
-    function displayTasks() {
-      const tasksContainer = document.getElementById("tasks");
-      tasksContainer.innerHTML = "";
-      tasks.forEach((task, index) => {
-        const taskDiv = document.createElement("div");
-        const taskInput = document.createElement("input");
-        taskInput.type = "checkbox";
-        taskInput.checked = task.completed;
-        taskInput.addEventListener("change", () => toggleCompleted(index));
-        const taskText = document.createElement("span");
-        taskText.textContent = task.text;
-        if (task.completed) {
-          taskText.classList.add("completed");
-        }
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
-        deleteButton.addEventListener("click", () => deleteTask(index));
-        taskDiv.appendChild(taskInput);
-        taskDiv.appendChild(taskText);
-        taskDiv.appendChild(deleteButton);
-        tasksContainer.appendChild(taskDiv);
-      });
-    }
-
-    // Function to toggle task completion
-    function toggleCompleted(index) {
-      tasks[index].completed = !tasks[index].completed;
-      displayTasks();
-    }
-
-    // Function to delete a task
-    function deleteTask(index) {
-      tasks.splice(index, 1);
-      displayTasks();
-    }
-
-    // Initial display of tasks
-    displayTasks();
-  
+// Function to simulate the checkout process
+function checkout() {
+  alert('Checkout Successful!\n\nTotal Amount: $XXX'); // Replace XXX with the actual total amount
+  // Reset the cart after checkout
+  for (const productId in cart) {
+    cart[productId] = 0;
+  }
+  const cartItems = document.getElementById('cart-items');
+  cartItems.innerHTML = '';
+}
+   
